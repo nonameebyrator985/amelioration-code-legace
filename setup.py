5bff22881b781e2473979d60874e430f173f44ef
nonameebyrator985 <singheingemo@hotmail.com>
2025-09-05 02:41:51 +0900

    Fix package list in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='amelioration_code_heire',
     version='0.1',
-    packages=[''],
+    packages=['src'],  # Updated packages list
     install_requires=[],
     entry_points={
         'console_scripts': ['ameliorer=main:main'],
