66191028604c367bacc90a303b12355a9d76ab18
nonameebyrator985 <singheingemo@hotmail.com>
2025-09-05 02:41:10 +0900

    feat: initial commit for setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='amelioration_code_heire',
+    version='0.1',
+    packages=[''],
+    install_requires=[],
+    entry_points={
+        'console_scripts': ['ameliorer=main:main'],
+    },
+)
